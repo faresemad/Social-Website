d18aae16f64803e56b6238620170ee921091bdff
Fares Emad <faresemadx@gmail.com>
2023-10-12 16:38:42 +0200

    using login and logout django

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,10 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 
-from .views import user_login
+# from .views import user_login
 
 urlpatterns = [
-    path("login/", user_login, name="user_login"),
+    # path("login/", user_login, name="user_login"),
+    path("login/", auth_views.LoginView.as_view(), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="ogout"),
 ]
