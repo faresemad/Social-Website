eb2c19302c48c9492508c8938d0aaf872c133556
Fares Emad <faresemadx@gmail.com>
2023-10-12 16:53:40 +0200

    create `dashboard` view and template

--- account/urls.py
+++ account/urls.py
@@ -1,10 +1,11 @@
 from django.contrib.auth import views as auth_views
 from django.urls import path
 
-# from .views import user_login
+from . import views
 
 urlpatterns = [
-    # path("login/", user_login, name="user_login"),
+    # path("login/", views.user_login, name="user_login"),
     path("login/", auth_views.LoginView.as_view(), name="login"),
-    path("logout/", auth_views.LogoutView.as_view(), name="ogout"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path("", views.dashboard, name="dashboard"),
 ]
