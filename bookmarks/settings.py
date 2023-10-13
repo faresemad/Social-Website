717f8021f43dc7c3d7f9378a14b3510249bfdaa0
Fares Emad <faresemadx@gmail.com>
2023-10-13 14:47:26 +0200

    Building a custom authentication backend now you can login with username or email

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -142,3 +142,8 @@ LOGIN_REDIRECT_URL = "dashboard"
 LOGIN_URL = "login"
 LOGOUT_URL = "logout"
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+AUTHENTICATION_BACKENDS = [
+    "django.contrib.auth.backends.ModelBackend",
+    "account.authentication.EmailAuthBackend",
+]
