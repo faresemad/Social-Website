a60c1542248d4e40151c8440bbc136a25919a1fd
Fares Emad <faresemadx@gmail.com>
2023-10-13 14:23:40 +0200

    extend the user model and create profile

--- account/forms.py
+++ account/forms.py
@@ -1,6 +1,20 @@
 from django import forms
 from django.contrib.auth.models import User
 
+from .models import Profile
+
+
+class UserEditForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ["first_name", "last_name", "email"]
+
+
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ["date_of_birth", "photo"]
+
 
 class LoginForm(forms.Form):
     username = forms.CharField(label="Username", max_length=100)
