a3b5c33b88f0b0130d5d84649e3a4cbb1a4944bd
Fares Emad <faresemadx@gmail.com>
2023-10-13 14:50:11 +0200

    set Preventing users from using an existing email on edit form

--- account/forms.py
+++ account/forms.py
@@ -9,6 +9,13 @@ class UserEditForm(forms.ModelForm):
         model = User
         fields = ["first_name", "last_name", "email"]
 
+    def clean_email(self):
+        data = self.cleaned_data["email"]
+        qs = User.objects.exclude(id=self.instance.id).filter(email=data)
+        if qs.exists():
+            raise forms.ValidationError(" Email already in use.")
+        return data
+
 
 class ProfileEditForm(forms.ModelForm):
     class Meta:
