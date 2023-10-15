a9b49dcd967168ec97842729e7d571b160778780
Fares Emad <faresemadx@gmail.com>
2023-10-15 13:00:01 +0200

    feat: Create `image_create` view and adding `URL` routing

--- bookmarks/urls.py
+++ bookmarks/urls.py
@@ -7,6 +7,10 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("account/", include("account.urls")),
     path("social-auth/", include("social_django.urls", namespace="social")),
+    path("images/", include("images.urls", namespace="images")),
 ]
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT,
+    )
--- images/views.py
+++ images/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render
 
-# Create your views here.
+from .forms import ImageCreateForm
+
+
+@login_required
+def image_create(request):
+    if request.method == "POST":
+        form = ImageCreateForm(request.POST)
+        if form.is_valid():
+            cb = form.changed_data  # noqa
+            new_item = form.save(commit=False)
+            # assign current user to the item
+            new_item.user = request.user
+            new_item.save()
+            messages.success(request, "Image added successfully.")
+            return redirect(new_item.get_absolute_url())
+    else:
+        form = ImageCreateForm(data=request.GET)
+    return render(
+        request,
+        "images/image/create.html",
+        {
+            "section": "images",
+            "form": form,
+        },
+    )
